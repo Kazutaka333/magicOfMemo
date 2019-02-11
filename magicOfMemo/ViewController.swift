cffdcc24465f52a3f75be9694c725b75c06ac102
Kazutaka Homma <special18fender@gmail.com>
2019-02-10 21:42:25 -0800

    Initial Commit

---
+++ magicOfMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magicOfMemo
+//
+//  Created by Kazutaka Homma on 2/10/19.
+//  Copyright © 2019 Kazutaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
